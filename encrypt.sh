482e5310bcf89b2e0df094f6f70ca9b6b2d52f73
goblininc <96318764+goblininc@users.noreply.github.com>
2021-12-17 23:26:11 +0100

    Removed password from encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "This is for encrypting the loot! Until we come up with our own encryptor, we will use 7zip. You will need to provide the following password: WichtelWichtel123!"
+echo "This is for encrypting the loot! Until we come up with our own encryptor, we will use 7zip. You will need to provide the our password!"

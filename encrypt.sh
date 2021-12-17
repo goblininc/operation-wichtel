63376eac0bd8ff36d39ae5577275f299f1768ce4
goblininc <96318764+goblininc@users.noreply.github.com>
2021-12-17 23:59:39 +0100

    Fixed typos

--- encrypt.sh
+++ encrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "This is for encrypting the loot! Until we come up with our own encryptor, we will use 7zip. You will need to provide the our password!"
+echo "This is for encrypting the loot! Until we come up with our own encryptor, we will use 7zip. You will need to provide the password!"
